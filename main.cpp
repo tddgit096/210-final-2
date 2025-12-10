0e4535bdefd19220e878c06e7898556bfdf55f6f
tddgit096-pc <>
2025-12-10 14:29:10 -0800

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+// Final Exam II | Toma Dimov | COMSC210
+#include <iostream>
+/*
+Your code must run at each milestone and exercise the structures or programming you just wrote
+
+If you paste in code from a previous assignment, you must:
+ (1) write a nearby comment in your code with attribution; and 
+ (2) do an immediate commit with a brief explanation in the commit comments
+*/
+
+/*MILESTONE 1 Using a struct, code a coffee booth queue simulation
+ that uses a linked list as its core data structure. The linked list's 
+ struct should be a node that contains the customer's name and drink order.
+ Create data arrays with names and drink orders from which to randomly create
+ customer data. You may use an LLM for creating these data sets. 
+*/
+using namespace std;
+
+int main(){
+    return 0;
+}
